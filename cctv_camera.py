f9dd818ad9adae1ba7f1f4baa3ffe5a9ba01db58
Ashwin <79413961+dev-ashwin7@users.noreply.github.com>
2023-07-23 18:17:42 +0530

    Add files via upload

---
+++ cctv_camera.py
@@ -0,0 +1,101 @@
+import cv2
+import numpy as np
+import base64
+import requests
+
+twilioPhoneNumber = '+12179088838'
+toPhoneNumber = '+916366060912'
+
+def handleCallButtonPress():
+    twilioAPIBaseUrl = 'https://api.twilio.com/2010-04-01'
+    accountSid = 'ACc00dd309196ccc63c81655e4fef9c573'
+    authToken = '5c5a829c20de074f5cb0d07303422559'
+    twilioPhoneNumberFormatted = requests.utils.quote(twilioPhoneNumber)
+    toPhoneNumberFormatted = requests.utils.quote(toPhoneNumber)
+    callUrl = f'{twilioAPIBaseUrl}/Accounts/{accountSid}/Calls.json'
+
+    authHeader = {
+        'Authorization': 'Basic ' + base64.b64encode(f'{accountSid}:{authToken}'.encode()).decode(),
+        'Content-Type': 'application/x-www-form-urlencoded',
+    }
+
+    formData = f'To={toPhoneNumberFormatted}&From={twilioPhoneNumberFormatted}&Url=https://handler.twilio.com/twiml/EH4a24acea3c5df1809a3232e5dfea9320'
+
+    response = requests.post(callUrl, headers=authHeader, data=formData)
+
+    if response.ok:
+        data = response.json()
+        sid = data.get('sid')
+    else:
+        print('Error making the call:', response.text)
+
+configs = cv2.dnn.readNet("yolov3_training_2000.weights", "yolov3_testing.cfg")
+configs.setPreferableBackend(cv2.dnn.DNN_BACKEND_DEFAULT)
+configs.setPreferableTarget(cv2.dnn.DNN_TARGET_CPU)
+classes = ["Weapon"]
+
+
+#gun 1: ./assets/gun-1.mp4
+#gun 2: ./assets/gun-2.mp4
+#no gun: ./assets/no_gun.mp4
+cap = cv2.VideoCapture("./assets/gun-2.mp4")
+
+while True:
+    try:
+        _, img = cap.read()
+        height, width, channels = img.shape
+        blob = cv2.dnn.blobFromImage(img, 0.00392, (416, 416), (0, 0, 0), True, crop=False)
+        configs.setInput(blob)
+        
+        layer_names = configs.getLayerNames()
+
+        output_layers = [layer_names[i - 1] for i in configs.getUnconnectedOutLayers()]
+        outs = configs.forward(output_layers)
+
+        
+        class_ids = []
+        accs = []
+        boxes = []
+        for out in outs:
+            for detection in out:
+                scores = detection[5:]
+                class_id = np.argmax(scores)
+                confidence = scores[class_id]
+                if confidence > 0.5:
+                    
+                    center_x = int(detection[0] * width)
+                    center_y = int(detection[1] * height)
+                    w = int(detection[2] * width)
+                    h = int(detection[3] * height)
+
+                    
+                    x = int(center_x - w / 2)
+                    y = int(center_y - h / 2)
+
+                    boxes.append([x, y, w, h])
+                    accs.append(float(confidence))
+                    class_ids.append(class_id)
+
+        vals = cv2.dnn.NMSBoxes(boxes, accs, 0.5, 0.4)
+        
+        for i in range(len(boxes)):
+            if i in vals:
+                x, y, w, h = boxes[i]
+                red = (0, 0, 255)
+                cv2.rectangle(img, (x, y), (x + w, y + h), red, 2)
+
+        
+        if vals == 0:
+            print("WEAPON FOUND")
+            cv2.imwrite("gun_found.jpg", img)
+            handleCallButtonPress()
+            break
+        key = cv2.waitKey(1)
+        if key == 27:
+            quit()
+    except:
+        print("NO WEAPON")
+        quit()
+
+cap.release()
+cv2.destroyAllWindows()
---
+++ gunDetectionCallerFirebase.py
@@ -0,0 +1,144 @@
+import numpy as np
+import matplotlib.pyplot as plt
+from scipy.io import wavfile
+import os
+import base64
+import requests
+import pyrebase
+
+twilioPhoneNumber = '+12179088838'
+toPhoneNumber = '+916366060912'
+
+
+firebaseConfig = {
+    "apiKey": "AIzaSyBMesKBiA-teWfY0yUm5ffbhQgm-rDisVY",
+    "authDomain": "stemisthacks2023.firebaseapp.com",
+    "projectId": "stemisthacks2023",
+    "storageBucket": "stemisthacks2023.appspot.com",
+    "messagingSenderId": "1067109385061",
+    "appId": "1:1067109385061:web:093bcd5f1fa42cbde5978c",
+    'databaseURL' : "https://stemisthacks2023-default-rtdb.firebaseio.com/"
+}
+
+firebase = pyrebase.initialize_app(firebaseConfig)
+database = firebase.database()
+
+def main(file_path):
+    sample_rate, signal = wavfile.read(file_path)
+
+    
+    if len(signal.shape) > 1:
+        signal = signal[:, 0]
+
+    
+    fft_result = np.fft.rfft(signal)
+    magnitudes = np.abs(fft_result)
+
+    
+    num_samples = len(signal)
+    
+    frequency_axis = np.fft.rfftfreq(num_samples, d=1.0 / sample_rate)
+  
+
+    
+    max_magnitude_index = np.argmax(magnitudes)
+    max_magnitude_frequency = frequency_axis[max_magnitude_index]
+
+    return max_magnitude_frequency
+
+def handleCallButtonPress():
+    twilioAPIBaseUrl = 'https://api.twilio.com/2010-04-01'
+    accountSid = 'ACc00dd309196ccc63c81655e4fef9c573'
+    authToken = '5c5a829c20de074f5cb0d07303422559'
+    twilioPhoneNumberFormatted = requests.utils.quote(twilioPhoneNumber)
+    toPhoneNumberFormatted = requests.utils.quote(toPhoneNumber)
+    callUrl = f'{twilioAPIBaseUrl}/Accounts/{accountSid}/Calls.json'
+
+    authHeader = {
+        'Authorization': 'Basic ' + base64.b64encode(f'{accountSid}:{authToken}'.encode()).decode(),
+        'Content-Type': 'application/x-www-form-urlencoded',
+    }
+
+    formData = f'To={toPhoneNumberFormatted}&From={twilioPhoneNumberFormatted}&Url=https://handler.twilio.com/twiml/EH4a24acea3c5df1809a3232e5dfea9320'
+
+    response = requests.post(callUrl, headers=authHeader, data=formData)
+
+    if response.ok:
+        data = response.json()
+        sid = data.get('sid')
+    else:
+        print('Error making the call:', response.text)
+
+'''
+with open("gunshot_freqs.txt", "w") as f:
+    f.write("")
+
+folders = os.listdir('C:\\Users\\ashwi\\OneDrive\\Documents\\hackathon prep\\hackathon\\gunshot_audio_2')
+for folder in folders:
+    with open("gunshot_freqs.txt", "a") as f:
+        f.write(folder + "\n")
+    li = []
+    files = os.listdir('C:\\Users\\ashwi\\OneDrive\\Documents\\hackathon prep\\hackathon\\gunshot_audio_2\\' + folder)
+    for file in files:
+        
+        x = main('C:\\Users\\ashwi\\OneDrive\\Documents\\hackathon prep\\hackathon\\gunshot_audio_2' + "\\" + folder + '\\' + file)
+        if(folder == "AK-12"):
+            #400 - 500
+            if(x >= 400 and x <= 500):
+                li.append(x)
+        elif(folder == "Zastava M92"):
+            #100 - 200
+            if(x >= 100 and x <= 200):
+                li.append(x)
+        elif(folder == "IMI Desert Eagle"):
+            #0 - 100
+            if(x >= 0 and x <= 100):
+                li.append(x)
+    with open("gunshot_freqs.txt", "a") as f:
+        for i in li:
+            f.write(str(i))
+            f.write("\n")
+        f.write("\n\n")
+    median = np.median(li)
+    print(folder)
+    print(median)
+'''
+
+#after analyzying the median values of the audio data - the values in the ranges dict have been selected as they provided the most accurate results
+#guns that are found:
+#AK-12
+#AK-47
+#M4
+#M16
+#M249
+#MG-42
+#MP5
+#Machine Gun
+
+ranges_dict = {
+    "AK" : [400, 700],
+    "Machine Gun" : [0, 200],
+}
+
+'''
+paths
+C:\\Users\\ashwi\\OneDrive\\Documents\\hackathon prep\\hackathon\\assets\\gunshot_audio_2\\AK-12\\3 (1).wav
+
+'''
+audio_file = "C:\\Users\\ashwi\\OneDrive\\Documents\\hackathon prep\\hackathon\\assets\\gunshot_audio_2\\AK-12\\3 (1).wav"
+x = main(audio_file)
+handleCallButtonPress()
+
+for i in ranges_dict:
+    lower, higher = ranges_dict[i]
+    if(x >= lower and x <= higher):
+        gun_type = i
+
+gunshot_location = database.child('locations').child('5036709156539269').child('location').get().val()
+
+data = {
+    'location' : gunshot_location,
+    'gun type' : gun_type
+}
+
+gunshot = database.child("Gunshot").set(data)
